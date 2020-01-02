e35f53fd7550b40846fd1ebace91ae3ed9efaf64
new-HyeonWoo <chlgusdn3@naver.com>
2020-01-02 23:11:13 +0900

    #108 등록/수정/조회 API 만들기

---
+++ src/main/java/com/hyeon/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.hyeon.book.springboot.service.posts;
+
+import com.hyeon.book.springboot.domain.posts.PostsRepository;
+import com.hyeon.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEmpty().getId());
+    }
+}
