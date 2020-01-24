1a9a9160da12cde6b3e5ca9c3f532c1d14651ccf
new-HyeonWoo <chlgusdn3@naver.com>
2020-01-24 20:30:34 +0900

    #194 스프링 시큐리티 OAuth2 로그인 기능 구현

--- src/main/java/com/hyeon/book/springboot/web/IndexController.java
+++ src/main/java/com/hyeon/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.hyeon.book.springboot.web;
 
+import com.hyeon.book.springboot.config.auth.dto.SessionUser;
+import com.hyeon.book.springboot.domain.user.User;
 import com.hyeon.book.springboot.service.PostsService;
 import com.hyeon.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private  final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
