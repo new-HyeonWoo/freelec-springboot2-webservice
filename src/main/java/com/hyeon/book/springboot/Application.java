8698e8d1c1f1bf8ee338f6c8c2e1d33b9b9645c8
new-HyeonWoo <chlgusdn3@naver.com>
2020-01-25 22:09:39 +0900

    #223 기존 테스트에 시큐리티 적용하기

--- src/main/java/com/hyeon/book/springboot/Application.java
+++ src/main/java/com/hyeon/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화
+//@EnableJpaAuditing // JPA Auditing 활성화 -> JapConfig 를 생성하여 @EnableJpaAuditing 추가한다.
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
